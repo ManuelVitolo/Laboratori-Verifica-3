8fa5c3545dcde57dd4417071198b2f064b319861
-_Lorenzo_- <127199516+LLorenzoLL@users.noreply.github.com>
2023-05-09 12:56:19 +0200

    Create Docenti.java

---
+++ Docenti.java
@@ -0,0 +1,18 @@
+public class Docenti {
+    private Docente[] lista;
+    private int i=0;
+    public Docenti(){
+       lista=new Docente[100];
+    }
+     public Docente [] getLista(){
+        return lista;
+     }
+     public void setLista(Docente[] lista){
+        this.lista=lista;
+       
+       public void aggDoc(Docente docente{
+         this.listaè[i]=docente;
+         i++
+       }
+    }
+}
