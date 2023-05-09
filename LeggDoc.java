6b6ae83aa4e1a85681f5ed8b7035ec6b9504ef29
-_Lorenzo_- <127199516+LLorenzoLL@users.noreply.github.com>
2023-05-09 13:06:05 +0200

    Create LeggDoc.java

---
+++ LeggDoc.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Verifica {
+    public class BufferReaderExample {
+    public static void main(String [] args) 
+        throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Docente.txt"));
+        Docenti ll=new Docenti();
+        String line = reader.readLine();
+        while(line!=null) {
+        System.out.println(line);
+        line = reader.readLine();
+        String[] f = line.split(";");
+        String f1 = l[0];
+        String f2 = l[1];
+        String f3 = l[2]; 
+        Docente f=new Docente(l1,l2,l3);
+        ff.aggDoc(f);
+    } 
+    } 
+    }
+        
+}
