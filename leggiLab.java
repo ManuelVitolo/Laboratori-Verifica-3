874ae62ae1b76f1e4b6ef04cf01516dd043cbffd
Germano <127199633+Germano126@users.noreply.github.com>
2023-05-09 13:01:55 +0200

    Create leggiLab.java

---
+++ leggiLab.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Verifica {
+    public class BufferReaderExample {
+    public static void main(String [] args) 
+        throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Laboratorio.txt"));
+        Laboratori ll=new Laboratori();
+        String line = reader.readLine();
+        while(line!=null) {
+        System.out.println(line);
+        line = reader.readLine();
+        String[] l = line.split(";");
+        String l1 = l[0];
+        String l2 = l[1];
+        String l3 = l[2]; 
+        Laboratorio l=new Laboratorio(l1,l2,l3);
+        ll.aggLab(l);
+    } 
+    } 
+    }
+        
+}
