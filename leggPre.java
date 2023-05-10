3464a86d13947d1f346dd44600cda7fc0d45ee4f
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-10 09:59:15 +0200

    Update leggPre.java

--- leggPre.java
+++ leggPre.java
@@ -3,20 +3,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Verifica {
+public class LeggPre {
     public class BufferReaderExample {
-    public static void main(String [] args) 
+    public static void LeggPre(String [] args) 
         throws FileNotFoundException, IOException {
         BufferedReader reader = new BufferedReader(new FileReader("Prenotazione.txt"));
-        Prenotazioni ll=new Prenotazioni();
+        Prenotazioni uu=new Prenotazioni();
         String line = reader.readLine();
         while(line!=null) {
         System.out.println(line);
         line = reader.readLine();
         String[]u = line.split(";");
-        String u1 = l[0];
-        String u2 = l[1];
-        String u3 = l[2]; 
+        String u1 = u[0];
+        String u2 = u[1];
+        String u3 = u[2]; 
       Prenotazioni u=new Prenotazione(u1,u2,u3);
         uu.aggPre(u);
     } 
