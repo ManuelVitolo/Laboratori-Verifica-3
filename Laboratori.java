9b2f525282509fb0c0c4832f4011f3f258628f2b
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-10 10:13:33 +0200

    Create Laboratori.java

---
+++ Laboratori.java
@@ -0,0 +1,18 @@
+public class Laboratori {
+    private Laboratorio [] lista;
+    private int i=0;
+    public Laboratori(){
+       lista=new Laboratorio[100];
+    }
+     public Laboratorio [] getLista(){
+        return lista;
+     }
+     public void setLista(Laboratorio[] lista){
+        this.lista=lista;
+    }
+    public void aggLab(Laboratorio laboratorio){
+        this.lista[i]=laboratorio;
+        i++;
+    }    
+    
+    }
