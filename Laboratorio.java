230d712afc14968474708355877071219107be4b
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-10 09:54:52 +0200

    Update Laboratorio.java

--- Laboratorio.java
+++ Laboratorio.java
@@ -9,13 +9,22 @@ package laboratorio;
  * @author Alunno
  */
 public class Laboratorio {
+private int id;
 private String piano;
 private String materia;
 
-public Laboratorio(String piano,String materia){
+public Laboratorio(int id, String piano,String materia){
+    setId(id);
     setPiano(piano);
     setMateria(materia);
    
+}
+
+public void setId(int id){
+    this.id=id;
+}
+public int getId(){
+    return id;
 }
 public void setPiano(String piano){
     this.piano=piano;
