b2e18b13ce961c4a19329a7d770149714742c8a4
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-10 10:17:10 +0200

    Create LeggPre.java

---
+++ LeggPre.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeggPre {
+    public class BufferReaderExample {
+    public static void LeggPre(String [] args) 
+        throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Prenotazione.txt"));
+        Prenotazioni uu=new Prenotazioni();
+        String line = reader.readLine();
+        while(line!=null) {
+        System.out.println(line);
+        line = reader.readLine();
+        String[]u = line.split(";");
+        String u1 = u[0];
+        String u2 = u[1];
+        String u3 = u[2]; 
+      Prenotazione u=new Prenotazione(u1,u2,u3);
+        uu.aggPre(u);
+    } 
+    } 
+    }
+        
+}
