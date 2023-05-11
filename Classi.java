52588fdbae038248557bd8375e2b6ead05b03bbc
Lattealcioccolato1 <127200123+Lattealcioccolato1@users.noreply.github.com>
2023-05-11 12:07:56 +0200

    Add files via upload

---
+++ Classi.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classi;
+
+/**
+ *
+ * @author Alunno
+ */
+public class Classi {
+
+    private Classe [] elenco;
+    private int i=0;
+    public Classi(){
+       elenco=new Classe[100];
+    }
+     public Classe [] getelenco(){
+        return elenco;
+     }
+     public void setElenco(Classe[] elenco){
+        this.elenco=elenco;
+    }
+    public void aggClas(Classe classe){
+        this.elenco[i]=Classe;
+        i++;
+    }    
+    
+    }
+    
+
