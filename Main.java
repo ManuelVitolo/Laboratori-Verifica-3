2e9abde55baf7b30ec754fa7c0a062d725c31e31
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-14 09:01:07 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter; 
+public class Main {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Laboratori n=new Laboratori();
+        Prenotazioni p=new Prenotazioni();
+        int scelta;
+        do{
+            System.out.println("Premi 0 per uscire, 1 per creare laboratori, 2 per vedere prenotazioni, 3 per inserire una data e vedere le prenotazioni fatte dagli altri di quel giorno");
+            scelta=sc.nextInt();
+            if(scelta==1){
+                
+            }else if(scelta==2){
+            p.stampaPre(); 
+            }else if(scelta==3){
+              
+            }
+        }while(scelta!=0);
+    }
+}
