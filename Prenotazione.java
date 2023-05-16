b74cc093a2371836e6433376c0414504cea1f446
Manuel <127199435+ManuelVitolo@users.noreply.github.com>
2023-05-16 12:17:57 +0200

    Update Prenotazione.java

--- Prenotazione.java
+++ Prenotazione.java
@@ -3,25 +3,25 @@ import java.time.format.DateTimeFormatter;
 
 public class Prenotazione{
     private LocalDateTime data;
-
-
-	public Prenotazione( int giorno,int mese, int anno, int ora, int min){
-            setData(giorno, mese, anno, ora, min);
+    private Docente docente;
+    private Classe classe;
+    private Laboratorio laboratorio;
+    
+    public Prenotazione( int giorno,int mese, int anno, int ora, int min){
+        setData(giorno, mese, anno, ora, min);
+            
            
 	}
         public LocalDateTime getData(){
 	return data;
 	}
 	public void setData(int giorno,int mese, int anno, int ora, int min){
-	this.data=LocalDateTime.of(anno, mese, giorno , ora, min);  ;
+	this.data=LocalDateTime.of(anno, mese, giorno , ora, min);  
 	}
 
 	public void formatta(){
-		LocalDateTime datetime1 = LocalDateTime.of(anno, mese, giorno , ora, min);  
-		  LocalDateTime datetime2 = datetime1.minusDays(100);  
-		  System.out.println("Before Formatting: " + datetime2);  
-		  DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");  
-		  String formatDateTime = datetime2.format(format);   
-		  System.out.println("After Formatting: " + formatDateTime );  
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");  
+		String formatDateTime = data.format(format);   
+		System.out.println("After Formatting: " + formatDateTime );  
 	}    
 }
